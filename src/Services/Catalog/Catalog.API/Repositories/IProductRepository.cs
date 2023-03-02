59249adc3254a9c125344c28e9936d473c3c860b
PragyanKaushiley <PragyanKaushiley@policybazaar.com>
2023-03-02 18:59:14 +0530

    Catalog Service

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.Entities;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        #region ReadOperations
+        Task<IEnumerable<Product>> GetProducts();
+
+        Task<Product> GetProduct(string id);
+
+        Task<IEnumerable<Product>> GetProductByName(string name);
+
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+        #endregion ReadOperations
+
+        Task CreateProduct(Product product);
+
+        Task<bool> UpdateProduct(Product product);
+
+        Task<bool> DeleteProduct(string id);
+    }
+}
